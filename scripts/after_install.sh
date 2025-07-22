b6e3bcd4fafc3ca2789135b7e9029d015a1bfb35
aravind30122003 <aravind30122003@gmail.com>
2025-07-23 02:04:15 +0530

    modified

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "===> Running AfterInstall..."
+
 cd /home/ec2-user/simplybyte_calculator-new || exit 1
-
-# Just list files for debug
 ls -la
-
-# Do not run npm install for now
-echo "AfterInstall completed successfully."
+echo "AfterInstall done."
